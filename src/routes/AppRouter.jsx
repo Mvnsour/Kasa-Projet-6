6534e45018685844f07e2f9ed41f6262b1e0fe12
Mansour Mbengue <mbgmansour13@gmail.com>
2025-05-01 12:15:42 +0200

    feat: refactor AppRouter to use Layout component and organize child routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,20 +1,27 @@
 import { createBrowserRouter } from 'react-router';
-import About from '../pages/About';
+import Layout from '../components/Layout';
 import Home from '../pages/Home';
+import About from '../pages/About';
 import NotFound from '../pages/NotFound';
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
     errorElement: <NotFound />,
-  },
-  {
-    path: "/about",
-    element: <About />
-  },
-  {
-    path: "*",
-    element: <NotFound />
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      }
+    ],
   }
 ]);
