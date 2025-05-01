e0382e94dbc9ecd3ca6cd8abcf3690e2c58ccc27
Mansour Mbengue <mbgmansour13@gmail.com>
2025-05-01 12:09:06 +0200

    feat: create Layout component and remove Navbar and Footer from About and Home components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
-import Navbar from "../components/Navbar";
 import HomeBanner from "../components/HomeBanner";
-import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
-      <Navbar />
       <HomeBanner />
-      <Footer />
     </>
   );
 }
