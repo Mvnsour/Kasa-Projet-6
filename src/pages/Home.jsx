4097efd6a9f0dfec464b744417a16c4ad88fa554
Mansour Mbengue <mbgmansour13@gmail.com>
2025-05-06 09:06:01 +0200

    feat: update HomeBanner styles and improve layout structure in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,20 @@
 import apartmentList from '../data/db.json';
 import HomeBanner from "../components/HomeBanner";
 import ApartmentCard from "../components/ApartmentCard";
+import './Home.scss';
 
 function Home() {
   return (
     <>
       <HomeBanner />
-      <ul>
-        {apartmentList.map((apartment) => (
-          <ApartmentCard
+        <ul className="apartment-list">
+          {apartmentList.map((apartment) => (
+            <ApartmentCard
             key={apartment.id}
             apartment={apartment}
-          />
-        ))}
-      </ul>
-
+            />
+          ))}
+        </ul>
     </>
   );
 }
